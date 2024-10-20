7054f7f95e49ea451401dd44b3150cd8246bee70
Yooval <116361337+Yooval@users.noreply.github.com>
2024-10-20 10:41:13 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
 
-const filterObj = (obj, ...allowedFields) => { // allowedFields is arr with all the arugments we passed that we wanna change
+const filterObj = (obj, ...allowedFields) => { 
     const newObj = {};
     Object.keys(obj).forEach(el => {
         if (allowedFields.includes(el)) newObj[el] = obj[el]; // in newOnj is all permitted fields
@@ -26,9 +26,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     }
     // 2) Filtered out unwanted fields names that arr not allowed to be updated.
 
-    const filteredBody = filterObj(req.body, 'name', 'email');// can change in body only name and email. cant change pass for exam
+    const filteredBody = filterObj(req.body, 'name', 'email');
     // 3) Update user document
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { // can use findByIdAndUpdate cause no dealing with sensitive fields like pass
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
         new: true, //return new doc
         runValidators: true
     });
@@ -42,7 +42,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user.id, { active: false }); //id is in req because it's in the jwt. we will user query middleware to not show unactive
+    await User.findByIdAndUpdate(req.user.id, { active: false }); 
 
     res.status(204).json({
         status: 'success',

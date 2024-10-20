c4109d5c625a510c9ae1c994c70561954b7af6eb
Yooval <116361337+Yooval@users.noreply.github.com>
2024-10-20 10:53:29 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -49,35 +49,32 @@ const userSchema = new mongoose.Schema({
         select: false
     }
 });
-userSchema.pre('save', async function (next) { //this is a middlewear. here we encrypt(hash) the password we inserted for security
-    //only run this pass if pass modified. this is the problematic code
-    if (!this.isModified('password')) return next();// encrypt only if the pass hasmodified(or created)) else just next and nothing will happen
-    this.password = await bcrypt.hash(this.password, 12); //12 is the solt. solt is the length of the code(string) added to the pass before encryption. now hashing is better.
-    this.passwordConfirm = undefined; // now it's not true cause it not updated. it's a require input so we will have to give it a value when we sign up and it will be validate.
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
     next();
 });
 
-userSchema.pre('save', function (next) { // before we save at the end of reset password in auth.
-    if (!this.isModified('password') || this.isNew) return next();// if pass didnt modified or new document do nothing
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password') || this.isNew) return next();
     this.passwordChangedAt = Date.now() - 1;
     next();
 });
 
 userSchema.pre(/^find/, function (next) {
-    // this point to current querry. before we do for example const users = await User.find() sobefore the find this middleware will run.
-    // before we do any find documents we wana find onlt active ones- this why this middleware.
     this.find({ active: { $ne: false } });
     next();
 });
 
-userSchema.methods.correctPassword = async function (candidatePassword, userPassword) { //bool.checks if the user password(already encrypted) similar to the candidate password(WHAT WE INSERTED AS PASSWORDCONFIRM)
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) { 
     return await bcrypt.compare(candidatePassword, userPassword);
 }
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     if (this.passwordChangedAt) { //if exist pass been changed
-        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);; //it's now in seconds. parse int means make it int. base 10.
-        return JWTTimestamp < changedTimestamp // JWTTimestamp is when we issued(addressed) the token if it was changed before(if changed at all) it's fine
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);;
+        return JWTTimestamp < changedTimestamp
     }
     return false;
 };
@@ -89,7 +86,7 @@ userSchema.methods.createPasswordResetToken = function () {
         .update(resetToken)
         .digest('hex');
 
-    this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // expires 10 minutes after was reset
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
 
     return resetToken;
 };

5ba110cc9ab17d9ff11a5ec1398a08bc33346c1d
Yooval <116361337+Yooval@users.noreply.github.com>
2024-10-20 11:01:50 +0300

    Update appError.js

--- utils/appError.js
+++ utils/appError.js
@@ -3,8 +3,8 @@ class AppError extends Error { //appError inherites from error
         super(message); // super calls the parent contructor(error)
 
         this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';// if the status code startwith 4 it must be a fail and otherewise it's an error.
-        this.isOperational = true; // all errors we create in this class are operetional error meaning an error that let as now what's wrong for example- user creating tour without a required field. we want to mark this kind of error with this property. if we would like to get all relevant errors it will let us
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true; // all errors we create in this class are operetional error.
 
         Error.captureStackTrace(this, this.constructor);
     }
